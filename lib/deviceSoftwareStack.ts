d5cf7095e03bffd0715c1c77879692be841c95f4
Tom Woodard <woodard.thomas@gmail.com>
2021-10-28 11:55:52 -0700

    minor update

--- lib/deviceSoftwareStack.ts
+++ lib/deviceSoftwareStack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket, BlockPublicAccess, HttpMethods, StorageClass } from '@aws-cdk/aws-s3';
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
-import { Function } from "@aws-cdk/aws-lambda";
+
 import { StringParameter } from '@aws-cdk/aws-ssm';
 import * as config from './config';
 
@@ -9,20 +9,18 @@ export class DeviceSoftwareStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    const lambdaArn = StringParameter.fromStringParameterAttributes(this, 'MyValue', {
-      parameterName: '/AlwaysOnward/lambdaArn',
+    const roleArn = StringParameter.fromStringParameterAttributes(this, 'MyValue', {
+      parameterName: '/AlwaysOnward/lambdaRoleArn',
     }).stringValue;
     
-    const handler = Function.fromFunctionArn(this, 'external-lambda-from-arn', 
-      lambdaArn
-    );
+    const role = iam.Role.fromRoleArn(this, "lambdaRole", roleArn);
     
     const sourceBucket = new Bucket(this, 'Always-Onward-deviceSoftware', {
       bucketName: 'always-onward-devicesoftware',
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     });
-    sourceBucket.grantRead(handler);
+    sourceBucket.grantRead(role);
 
     new BucketDeployment(this, 'DeployDeviceSoftware', {
       sources: [Source.asset(config.destDir)],
